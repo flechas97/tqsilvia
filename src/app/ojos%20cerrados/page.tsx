c3d96f84794292e14c60c36496abb4f3aa6e6585
flechas97 <flechasarcon@gmail.com>
2025-04-22 20:41:19 +0200

    cambio lugares

--- src/app/ojos%20cerrados/page.tsx
+++ src/app/ojos%20cerrados/page.tsx
@@ -1,14 +1,14 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useEffect } from "react";
+import {useEffect} from "react";
 export default function Home() {
-  useEffect(() => {
-    document.title = "Eventos" ; 
-  },[])
-  return (
-    <>
-      <h1>ojos cerrados</h1>
-    </>
- 
-  );
+    useEffect(() => {
+        document.title = "Eventos";
+    }, []);
+    return (
+        <>
+            <h1>ojos cerrados</h1>
+        </>
+    );
 }
