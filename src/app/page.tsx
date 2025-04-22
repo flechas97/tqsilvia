9d33cd58a94a4d344609db8b89d0fc6060200b90
flechas97 <flechasarcon@gmail.com>
2025-04-22 20:22:03 +0200

    cambios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,120 +1,132 @@
 "use client";
-import { useEffect, useState } from "react";
-import Link from 'next/link';
+import {useEffect, useState} from "react";
+import Link from "next/link";
 
 export default function Home() {
-  const [albums, setAlbums] = useState<any[]>([]);
-  const apiKey = 'ca89d86c9fbfedbd4794147b6604f361';
-  const username = 'roberto.bermejo97';
+    const [albums, setAlbums] = useState<any[]>([]);
+    const apiKey = "ca89d86c9fbfedbd4794147b6604f361";
+    const username = "roberto.bermejo97";
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Obtener el ID del usuario
-        const userId = await fetchUserId(apiKey, username);
-        if (userId) {
-          // Obtener los álbumes del usuario
-          const albumData = await fetchUserAlbums(apiKey, userId);
-          // Para cada álbum, obtener la primera foto
-          const albumsWithCover = await Promise.all(
-            albumData.map(async (album: any) => {
-              const coverPhoto = await fetchCoverPhoto(apiKey, album.id);
-              return {
-                ...album,
-                coverPhoto
-              };
-            })
-          );
-          setAlbums(albumsWithCover);
-        } else {
-          console.error('Could not fetch user ID');
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Obtener el ID del usuario
+                const userId = await fetchUserId(apiKey, username);
+                if (userId) {
+                    // Obtener los álbumes del usuario
+                    const albumData = await fetchUserAlbums(apiKey, userId);
+                    // Para cada álbum, obtener la primera foto
+                    const albumsWithCover = await Promise.all(
+                        albumData.map(async (album: any) => {
+                            const coverPhoto = await fetchCoverPhoto(
+                                apiKey,
+                                album.id
+                            );
+                            return {
+                                ...album,
+                                coverPhoto,
+                            };
+                        })
+                    );
+                    setAlbums(albumsWithCover);
+                } else {
+                    console.error("Could not fetch user ID");
+                }
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []); // El array vacío asegura que useEffect se ejecute solo una vez al montar el componente
+
+    async function fetchUserId(apiKey: string, username: string) {
+        const url = `https://www.flickr.com/services/rest/?method=flickr.people.findByUsername&api_key=${apiKey}&username=${username}&format=json&nojsoncallback=1`;
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data.user.nsid;
+        } catch (error) {
+            console.error("Error fetching user ID:", error);
+            return null;
         }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []); // El array vacío asegura que useEffect se ejecute solo una vez al montar el componente
-
-  async function fetchUserId(apiKey: string, username: string) {
-    const url = `https://www.flickr.com/services/rest/?method=flickr.people.findByUsername&api_key=${apiKey}&username=${username}&format=json&nojsoncallback=1`;
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      return data.user.nsid;
-    } catch (error) {
-      console.error('Error fetching user ID:', error);
-      return null;
     }
-  }
 
-  async function fetchUserAlbums(apiKey: string, userId: string) {
-    const url = `https://www.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=${apiKey}&user_id=${userId}&format=json&nojsoncallback=1`;
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      return data.photosets.photoset || [];
-    } catch (error) {
-      console.error('Error fetching user albums:', error);
-      return [];
+    async function fetchUserAlbums(apiKey: string, userId: string) {
+        const url = `https://www.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=${apiKey}&user_id=${userId}&format=json&nojsoncallback=1`;
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data.photosets.photoset || [];
+        } catch (error) {
+            console.error("Error fetching user albums:", error);
+            return [];
+        }
     }
-  }
 
-  async function fetchCoverPhoto(apiKey: string, albumId: string) {
-    const url = `https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${apiKey}&photoset_id=${albumId}&format=json&nojsoncallback=1`;
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      if (data.photoset.photo.length > 0) {
-        const firstPhoto = data.photoset.photo[0];
-        return `https://live.staticflickr.com/${firstPhoto.server}/${firstPhoto.id}_${firstPhoto.secret}_b.jpg`; // 'b' para una imagen de tamaño mediano
-      }
-      return null;
-    } catch (error) {
-      console.error('Error fetching cover photo:', error);
-      return null;
+    async function fetchCoverPhoto(apiKey: string, albumId: string) {
+        const url = `https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${apiKey}&photoset_id=${albumId}&format=json&nojsoncallback=1`;
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            if (data.photoset.photo.length > 0) {
+                const firstPhoto = data.photoset.photo[0];
+                return `https://live.staticflickr.com/${firstPhoto.server}/${firstPhoto.id}_${firstPhoto.secret}_b.jpg`; // 'b' para una imagen de tamaño mediano
+            }
+            return null;
+        } catch (error) {
+            console.error("Error fetching cover photo:", error);
+            return null;
+        }
     }
-  }
 
-  return (
-    <>
-      <div className="wrapperContent">
-        <h1>Últimas Publicaciones</h1>
-      </div>
-      <div className="albums">
-        {albums.length > 0 ? (
-          albums.map((album: any) => (
-            <div className="hrwrapper">
-            <div key={album.id} className="album">
-              <div className="album-cover">
-                {album.coverPhoto ? (
-                  <img src={album.coverPhoto} alt={`Cover of ${album.title._content}`} />
+    return (
+        <>
+            <div className="wrapperContent">
+                <h1>Últimas Publicaciones</h1>
+                <p>
+                    Bienvenido a la galería de fotos de Marta y Roberto. Un
+                    album donde ver el bonito amor que compartimos.
+                    <br />
+                    <br />
+                    Desde el 8 de noviembre de 2024, hemos compartido momentos
+                    inolvidables juntos.
+                </p>
+            </div>
+            <div className="albums">
+                {albums.length > 0 ? (
+                    albums.map((album: any) => (
+                        <div className="hrwrapper">
+                            <div key={album.id} className="album">
+                                <div className="album-cover">
+                                    {album.coverPhoto ? (
+                                        <img
+                                            src={album.coverPhoto}
+                                            alt={`Cover of ${album.title._content}`}
+                                        />
+                                    ) : (
+                                        <div>No Cover Photo</div>
+                                    )}
+                                </div>
+                                <div className="wrapperinfo">
+                                    <h2>{album.title._content}</h2>
+                                    <p>{album.description._content}</p>
+                                    <div className="btnverWrapper">
+                                        <Link href={`/albunes/${album.id}`}>
+                                            <button className="btnVerFotos">
+                                                Ver Fotos
+                                            </button>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr />
+                        </div>
+                    ))
                 ) : (
-                  <div>No Cover Photo</div>
+                    <p>Cargando...</p>
                 )}
-              </div>
-              <div className="wrapperinfo">
-              <h2>{album.title._content}</h2>
-              <p>{album.description._content}</p>
-              <div className="btnverWrapper">
-                <Link href={`/albunes/${album.id}`}>
-                    <button className="btnVerFotos">Ver Fotos</button>
-                </Link>
-              </div>
-           
-          
-              </div>
-          
             </div>
-            <hr />
-            </div>
-          ))
-        ) : (
-          <p>Cargando...</p>
-        )}
-      </div>
-     
-    </>
-  );
+        </>
+    );
 }
--- src/comps/Header.tsx
+++ src/comps/Header.tsx
@@ -1,58 +1,65 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 
-
 export default function Header() {
-    const items = ["Lugares", "Eventos", "Ojos Cerrados"];
+    const items = [
+        /*"Lugares", "Eventos", "Ojos Cerrados"*/
+        "Lugares",
+    ];
 
     function openNav() {
         const navMob = document.querySelector(".navMob");
 
-        navMob?.classList.toggle('openMenu');
-      
+        navMob?.classList.toggle("openMenu");
     }
 
     return (
-    <header>
-        <div className="wrapheader">
-            <div>
-                <h1>Álbum Silvia y Roberto</h1>
-            </div>
-            <div>
+        <header>
+            <div className="wrapheader">
+                <div>
+                    <h1>Álbum Marta y Roberto</h1>
+                </div>
+                <div>
+                    <button onClick={openNav} className="openNavMob">
+                        <Image
+                            src="/menu.png"
+                            alt="menu"
+                            width={40}
+                            height={40}
+                        />
+                    </button>
 
-                <button onClick={openNav} className="openNavMob">
-                    <Image src="/menu.png" alt="menu" width={40} height={40} />
-                </button>
-
-                <nav className="navMob">
-                    <ul>
-                        <li>
-                            <a href="/">Inicio</a>
-                        </li>
-                        {items.map((item) => (
-                            <li key={item}>
-                                <a href={`/${item.toLowerCase()}`}>{item}</a>
+                    <nav className="navMob">
+                        <ul>
+                            <li>
+                                <a href="/">Inicio</a>
                             </li>
-                        ))}
-                    </ul>
-                </nav>
-                <nav className="navDesk">
-                    <ul>
-                        <li>
-                            <a href="/">Inicio</a>
-                        </li>
-                        {items.map((item) => (
-                            <li key={item}>
-                                <a href={`/${item.toLowerCase()}`}>{item}</a>
+                            {items.map(item => (
+                                <li key={item}>
+                                    <a href={`/${item.toLowerCase()}`}>
+                                        {item}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                    <nav className="navDesk">
+                        <ul>
+                            <li>
+                                <a href="/">Inicio</a>
                             </li>
-                        ))}
-                    </ul>
-                </nav>
-
-
+                            {items.map(item => (
+                                <li key={item}>
+                                    <a href={`/${item.toLowerCase()}`}>
+                                        {item}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                </div>
             </div>
-        </div>
-    </header>
+        </header>
     );
 }
