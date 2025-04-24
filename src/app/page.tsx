854b59105e3727dba9dcdcd3a35870129e083645
flechas97 <flechasarcon@gmail.com>
2025-04-24 16:45:44 +0200

    cuanta atras

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,32 @@ export default function Home() {
     const [albums, setAlbums] = useState<any[]>([]);
     const apiKey = "ca89d86c9fbfedbd4794147b6604f361";
     const username = "roberto.bermejo97";
+    const [showContent, setShowContent] = useState(false);
+    const [countdown, setCountdown] = useState("");
+
+    const targetDate = new Date("2025-04-28T00:00:00"); // <-- AJUSTA ESTA FECHA
+    // const targetDate = new Date("2024-05-01T18:00:00"); // <-- AJUSTA ESTA FECHA
+    useEffect(() => {
+        const checkDate = () => {
+            const now = new Date();
+            const timeLeft = targetDate.getTime() - now.getTime();
+            if (timeLeft <= 0) {
+                setShowContent(true);
+            } else {
+                const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+                const hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+                const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+                const seconds = Math.floor((timeLeft / 1000) % 60);
+
+                setCountdown(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+            }
+        };
+
+        const interval = setInterval(checkDate, 1000);
+        checkDate(); // llama una vez al principio
+
+        return () => clearInterval(interval);
+    }, []);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -82,51 +108,72 @@ export default function Home() {
 
     return (
         <>
-            <div className="wrapperContent">
-                <h1>Últimas Publicaciones</h1>
-                <p>
-                    Bienvenido a la galería de fotos de Marta y Roberto. Un
-                    album donde ver el bonito amor que compartimos.
-                    <br />
-                    <br />
-                    Desde el 8 de noviembre de 2024, hemos compartido momentos
-                    inolvidables juntos.
-                </p>
-            </div>
-            <div className="albums">
-                {albums.length > 0 ? (
-                    albums.map((album: any) => (
-                        <div className="hrwrapper">
-                            <div key={album.id} className="album">
-                                <div className="album-cover">
-                                    {album.coverPhoto ? (
-                                        <img
-                                            src={album.coverPhoto}
-                                            alt={`Cover of ${album.title._content}`}
-                                        />
-                                    ) : (
-                                        <div>No Cover Photo</div>
-                                    )}
-                                </div>
-                                <div className="wrapperinfo">
-                                    <h2>{album.title._content}</h2>
-                                    <p>{album.description._content}</p>
-                                    <div className="btnverWrapper">
-                                        <Link href={`/albunes/${album.id}`}>
-                                            <button className="btnVerFotos">
-                                                Ver Fotos
-                                            </button>
-                                        </Link>
+            {!showContent && (
+                <div className="overlay">
+                    <div className="countdown">
+                        <h1>Hola Marta</h1>
+                        <img
+                            src="https://images.vexels.com/media/users/3/136172/isolated/preview/148ec098a4529de7141003a8ec519d39-corazon-como-icono.png"
+                            alt=""
+                            width={100}
+                        />
+                        <h2>Contando los días para el gran momento...</h2>
+                        <p>{countdown}</p>
+                    </div>
+                </div>
+            )}
+
+            {showContent && (
+                <>
+                    <div className="wrapperContent">
+                        <h1>Últimas Publicaciones</h1>
+                        <p>
+                            Bienvenido a la galería de fotos de Marta y Roberto.
+                            Un álbum donde ver el bonito amor que compartimos.
+                            <br />
+                            <br />
+                            Desde el 8 de noviembre de 2024, hemos compartido
+                            momentos inolvidables juntos.
+                        </p>
+                    </div>
+                    <div className="albums">
+                        {albums.length > 0 ? (
+                            albums.map((album: any) => (
+                                <div className="hrwrapper" key={album.id}>
+                                    <div className="album">
+                                        <div className="album-cover">
+                                            {album.coverPhoto ? (
+                                                <img
+                                                    src={album.coverPhoto}
+                                                    alt={`Cover of ${album.title._content}`}
+                                                />
+                                            ) : (
+                                                <div>No Cover Photo</div>
+                                            )}
+                                        </div>
+                                        <div className="wrapperinfo">
+                                            <h2>{album.title._content}</h2>
+                                            <p>{album.description._content}</p>
+                                            <div className="btnverWrapper">
+                                                <Link
+                                                    href={`/albunes/${album.id}`}
+                                                >
+                                                    <button className="btnVerFotos">
+                                                        Ver Fotos
+                                                    </button>
+                                                </Link>
+                                            </div>
+                                        </div>
                                     </div>
+                                    <hr />
                                 </div>
-                            </div>
-                            <hr />
-                        </div>
-                    ))
-                ) : (
-                    <p>Cargando...</p>
-                )}
-            </div>
+                            ))
+                        ) : (
+                            <p>Cargando...</p>
+                        )}
+                    </div>
+                </>
+            )}
         </>
     );
 }
