c2e31bc96401067d950bec808cdc1a102adbc845
flechas97 <flechasarcon@gmail.com>
2025-04-24 16:53:46 +0200

    cambio titulo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,31 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import Header from "../comps/Header";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
-import { GoogleAnalytics } from '@next/third-parties/google'
+import {Analytics} from "@vercel/analytics/react";
+import {GoogleAnalytics} from "@next/third-parties/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Álbum de Recuerdos Silvia y Roberto",
-  description: "Con amor, Silvia y Roberto",
+    title: "¿Que sera?",
+    // Álbum de Recuerdos Marta y Roberto
+    // description: "Con amor, marta y Roberto",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-      <Header />
-      <main>
-        {children}
-      </main>
-      <Analytics/>
-      <GoogleAnalytics gaId="G-EDFC79T6WZ" />
-        </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Header />
+                <main>{children}</main>
+                <Analytics />
+                <GoogleAnalytics gaId="G-EDFC79T6WZ" />
+            </body>
+        </html>
+    );
 }
